6565e85ea38fbd2527f6090a8345542663c9a7d1
Christopher <krzysiek-smaga@outlook.com>
2024-09-24 10:14:20 +0200

    Poprawny plik + Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
 import './App.css';
+
+// Tworzenie tablicy string
+const kursy = [
+  "Programowanie w C#",
+  "Angular dla początkujących",
+  "Kurs Django"
+];
+
+function LiczbaKursow(){
+  return (
+      <h2>Liczba kursów: {kursy.length}</h2>
+  );
+}
+
+function ListaKursow(){
+  const listaKursow = kursy.map(kurs => 
+    <li>{kurs}</li> 
+  );
+
+  return (
+    <ul>
+      {listaKursow}
+    </ul>
+  );
+}
+
+function Formularz(){
+  const [nazwa, setNazwa] = useState("");
+  const [numer, setNumer] = useState(0);
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      Zatwierdz(nazwa, numer);
+    }}>
+      <label for="imie">Imię i nazwisko</label><br />
+      <input type='text' id='imie' onChange={ e => setNazwa(e.target.value) } /><br />
+      <label for="numer">Imię i nazwisko</label><br />
+      <input type='number' id='numer' onChange={ e => setNumer(e.target.value) } /><br />
+      <button type='submit'>Zatwierdź</button>
+    </form>
+  );
+
+  
+}
+
+function Zatwierdz(nazwa, numer){
+  console.log(nazwa);
+  console.log(kursy[numer - 1]);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LiczbaKursow />
+      <ListaKursow />
+      <Formularz />
+    </>
   );
 }
 
